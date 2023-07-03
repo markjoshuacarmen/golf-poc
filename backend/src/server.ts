dbf459d35b4b0e4d12ae08804a47c1d9b88bbb11
Sandrex Endino <Sandrex.Cayle.Endino@ph.ey.com>
2023-07-03 10:48:57 +0800

    Implement basic MongoDB connection

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,6 +4,7 @@ import dotenv from 'dotenv'
 dotenv.config
 
 // db
+import connectDB from './db/connect'
 
 // routers
 
@@ -18,3 +19,21 @@ app.get('/', (req, res) => {
     // throw new Error()
 })
 
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
+
+const port = process.env.PORT || 5000
+const dbURL = 'mongodb+srv://sandrex:VfoIzaQVfoETZ0tf@nodeexpressprojects.nqofbn1.mongodb.net/POC-MERN?retryWrites=true&w=majority'
+
+const start = async () => {
+    try {
+        await connectDB(dbURL)
+        app.listen(port, () => {
+            console.log(`Server is listening on port ${port}...`)
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
