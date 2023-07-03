527ec34976bcb7e5882c85000bf946423167c87a
Sandrex Endino <Sandrex.Cayle.Endino@ph.ey.com>
2023-07-03 10:23:07 +0800

    Implement middlewares

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,1 +1,20 @@
-console.log("test")
+import express from 'express'
+const app = express()
+import dotenv from 'dotenv'
+dotenv.config
+
+// db
+
+// routers
+
+// middlewares
+import notFoundMiddleware from './middleware/not-found'
+import errorHandlerMiddleware from './middleware/error-handler'
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+    // throw new Error()
+})
+
